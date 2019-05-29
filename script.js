3280613b101bfdd4c8081b8c36c6bd0816099096
spencerchurchill <spencer.churchill@outlook.com>
2019-05-29 09:57:04 -0700

    removed autoscroll for manual scroll option along with code cleanup

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-function scroll(id, i) {
-    setTimeout(function() {
-        scrolling = setInterval(function() {
-            document.getElementsByClassName("summar_" + String(id))[i].scrollBy(0, 1);
-        }, 50);
-    }, 5000);
+function clear() {
+    // clear all divs
+    for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
+        for (let j = 1; j < document.getElementsByClassName("summar_"+String(i)).length; j++) {
+            document.getElementsByClassName("summar_" + String(i))[j].parentNode.removeChild(document.getElementsByClassName("summar_"+String(i))[j]);
+        }
+    }
+    for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
+        if (document.getElementsByClassName("summar_" + String(i))[0]) {
+            document.getElementsByClassName("summar_" + String(i))[0].style.visibility = "hidden";
+        }
+    }
 }
 
-function hide() {
-    // hide all divs
-    clearInterval(scrolling);
-    for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
-        for (let j = 0; j < document.getElementsByClassName("summar_"+String(i)).length; j++) {
-            document.getElementsByClassName("summar_"+String(i))[j].style.visibility = "hidden";
-            document.getElementsByClassName("summar_"+String(i))[j].scrollTop = 0;
-        };
-    };
-};
-
 function load(url, response, id) {
-    hide();
-    let sl = response.status.remaining_credits;
-    if (response.summary != undefined && sl >= 10) {
+    clear();
+    if (response.summary != undefined && response.status.remaining_credits >= 10) {
         let div = document.createElement("div");
         div.className = "summar_" + String(id);
         // display summary in div
         div.innerHTML = response.summary;
-        console.info("Remaing credits: ", sl);
-        chrome.storage.sync.set({"sl": sl});
+        // console.info("Remaing credits: ", response.status.remaining_credits);
+        chrome.storage.sync.set({"sl": response.status.remaining_credits});
         document.getElementsByTagName('a')[id].parentElement.appendChild(div);
     }
     else {
@@ -37,19 +31,13 @@ function load(url, response, id) {
         document.getElementsByTagName('a')[id].parentElement.appendChild(frame);
     }
     // set div styles
-    for (let i = 0; i < document.getElementsByClassName("summar_" + String(id)).length; i++) {
-        document.getElementsByClassName("summar_" + String(id))[i].style = "position: absolute; width: 300px; height: 150px; margin: 5px; background-color: rgba(255, 255, 255, 1); box-shadow: 0px 0px 10px grey; font-style: italic; font-size: 10pt; overflow: auto; zIndex: 2147483647; visibility: visible;";
-        scroll(id, i);
-    };
-};
+    document.getElementsByClassName("summar_" + String(id))[0].style = "position: absolute; width: 300px; height: 150px; margin: 5px; padding: 10px; background-color: rgba(255, 255, 255, 1); box-shadow: 0px 0px 10px grey; font: italic 10pt Times; overflow: auto; zIndex: 2147483647; visibility: visible;";
+}
 
 function summar(url, id) {
     // if the summary has already been loaded, make that div visible
-    if (document.getElementsByClassName("summar_" + String(id)).length > 0) {
-        for (let i = 0; i < document.getElementsByClassName("summar_" + String(id)).length; i++) {
-            document.getElementsByClassName("summar_" + String(id))[i].style.visibility = "visible";
-            scroll(id, i);
-        };
+    if (document.getElementsByClassName("summar_" + String(id))[0]) {
+        document.getElementsByClassName("summar_" + String(id))[0].style.visibility = "visible";
     }
     else {
         let settings = {
@@ -65,13 +53,13 @@ function summar(url, id) {
                 "url": url,
                 "sentences": "3"
             }
-        }
+        };
         $.ajax(settings).done(function (response) {
             // use summary
             load(url, response, id);
         });
-    };
-};
+    }
+}
 
 function modsums() {
     for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
@@ -85,17 +73,21 @@ function modsums() {
             }, 1700); // wait about 2 seconds before calling summary function
         };
         tag.onmouseout = function() {
-            // on mouseout, hide all summary boxes
-            hide();
+            let notHover = setInterval(function() {
+                if (tag.parentElement.querySelector(":hover") != document.getElementsByClassName("summar_" + String(i))[0]) {
+                    // on mouseout, clear all summary boxes
+                    clear();
+                    clearInterval(notHover);
+                }
+            }, 1700); // wait about 2 seconds before calling summary function
         };
-    };
-};
+    }
+}
 
 function on_load() {
     chrome.storage.sync.get(["key"], function(result) {
         key = result.key;
     });
-
     chrome.storage.sync.get(["whitelist"], function(result) {
         if (urls) {
             urls = result.whitelist;
@@ -104,15 +96,14 @@ function on_load() {
             urls = "";
         }
     });
-
     // timeout to allow time for chrome to sync url values
     setTimeout(function() {
         if (urls.includes(document.URL.split(".")[1]) === false) {
             modsums();
         }
     }, 250);
-};
+}
 
-let key, urls, scrolling;
+let key, urls;
 
 on_load();
