8622d94dc35a25d8692ea20e19de96c27a3ad37c
spencerchurchill <spencer.churchill@outlook.com>
2019-06-01 00:01:35 -0700

    standard variable naming

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-console.time("Sumit initialization"); // measure lag (1, 92)
+console.time("Sumit initialization"); // measure lag Ln {1, 94}
 
 let apiID = apiKey = urls = '';
 
 function clearDivs() {
     // clear all divs
     for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
-        for (let j = 1; j < document.getElementsByClassName("sumit_" + String(i)).length; j++) {
-            document.getElementsByClassName("sumit_" + String(i))[j].parentNode.removeChild(document.getElementsByClassName("sumit_" + String(i))[j]);
+        for (let j = 1; j < document.getElementsByClassName("sumit" + String(i)).length; j++) {
+            document.getElementsByClassName("sumit" + String(i))[j].parentNode.removeChild(document.getElementsByClassName("sumit" + String(i))[j]);
         }
     }
     for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
-        if (document.getElementsByClassName("sumit_" + String(i))[0]) {
-            document.getElementsByClassName("sumit_" + String(i))[0].style.visibility = "hidden";
-            document.getElementsByClassName("sumit_" + String(i))[0].scrollTop = 0;
+        if (document.getElementsByClassName("sumit" + String(i))[0]) {
+            document.getElementsByClassName("sumit" + String(i))[0].style.visibility = "hidden";
+            document.getElementsByClassName("sumit" + String(i))[0].scrollTop = 0;
         }
     }
 }
@@ -31,7 +31,7 @@ function createDivs(url, summary, id) {
         div = document.createElement("iframe");
         div.src = url;
     }
-    div.className = "sumit_" + String(id);
+    div.className = "sumit" + String(id);
     // set div styles
     div.style = "position: absolute !important; width: 300px; max-height: 250px; margin: 5px; padding: 10px; background-color: rgba(255, 255, 255, 1) !important; box-shadow: 0px 0px 10px grey; font: italic 10pt Times !important; overflow: auto; zIndex: 10000000 !important; visibility: visible;";
     document.getElementsByTagName('a')[id].parentElement.appendChild(div);
@@ -40,8 +40,8 @@ function createDivs(url, summary, id) {
 function summarize(url, id) {
     let summary;
     // if the summary has already been loaded, make that div visible
-    if (document.getElementsByClassName("sumit_" + String(id))[0]) {
-        document.getElementsByClassName("sumit_" + String(id))[0].style.visibility = "visible";
+    if (document.getElementsByClassName("sumit" + String(id))[0]) {
+        document.getElementsByClassName("sumit" + String(id))[0].style.visibility = "visible";
     }
     else {
         let settings = {
@@ -59,9 +59,9 @@ function summarize(url, id) {
             }
         };
         $.ajax(settings).always(function (result) {
-            // use summary
+            // create div regardless of summary
             if (result.sentences) {
-                summary = result.sentences.slice(0, 3).join(' ');
+                summary = result.sentences.slice(0, 3).join(' '); // 3 sentence summary
             }
             createDivs(url, summary, id);
         });
@@ -79,16 +79,16 @@ function addFunction() {
                         summarize(tag.href, i);
                     }
                 }
-            }, 1400); // wait about 2 seconds before calling summary function
+            }, 1500); // wait about 2 seconds before calling summary function
         };
         tag.onmouseout = function() {
             let notHover = setInterval(function() {
-                if (tag.parentElement.querySelector(":hover") !== document.getElementsByClassName("sumit_" + String(i))[0]) {
+                if (tag.parentElement.querySelector(":hover") !== document.getElementsByClassName("sumit" + String(i))[0]) {
                     // on mouseout, clear all summary boxes
                     clearDivs();
                     clearInterval(notHover);
                 }
-            }, 1300); // wait less time than summary before clearing
+            }, 1400); // wait less time than summary before clearing
         };
     }
     console.timeEnd("Sumit initialization");
