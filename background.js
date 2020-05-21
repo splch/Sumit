1123f21b74000823e26703e1a3c6dc9a2eee6a45
spencerchurchill <spencer.churchill@outlook.com>
2020-05-20 23:05:18 -0700

    Functionality improvements
    Whitelist fix

--- background.js
+++ background.js
@@ -44,8 +44,8 @@ function summarize(url, id) {
     if (document.getElementsByClassName("sumit" + String(id))[0]) {
         document.getElementsByClassName("sumit" + String(id))[0].style.visibility = "visible";
     }
-    // when there aren't many searches left, load iframe
-    else if (cl <= 10) {
+    // load iframe when credits are low
+    else if (cl < 10) {
         createDivs(url, summary, id);
     }
     else {
@@ -68,7 +68,7 @@ function summarize(url, id) {
             // create div regardless of summary
             if (err === "success" && i === 0) {
                 summary = result.sentences.join(' ');
-                chrome.storage.sync.set({"cl": limit.getResponseHeader("X-RateLimit-Remaining")});
+                chrome.storage.sync.set({ "cl": limit.getResponseHeader("X-RateLimit-Remaining") });
             }
             createDivs(url, summary, id);
             i++; // i prevents two requests from occuring
@@ -79,18 +79,18 @@ function summarize(url, id) {
 function addFunction() {
     for (let i = 0; i < document.getElementsByTagName('a').length; i++) {
         let tag = document.getElementsByTagName('a')[i];
-        tag.onmouseenter = function() {
+        tag.onmouseenter = function () {
             // on hover, send url to summarization function
-            setTimeout(function() {
+            setTimeout(function () {
                 if (tag.parentElement.querySelector(":hover") === tag) {
-                    if(tag.href) {
+                    if (tag.href) {
                         summarize(tag.href, i);
                     }
                 }
             }, 1500); // wait about 2 seconds before calling summary function
         };
-        tag.onmouseout = function() {
-            let notHover = setInterval(function() {
+        tag.onmouseout = function () {
+            let notHover = setInterval(function () {
                 if (tag.parentElement.querySelector(":hover") !== document.getElementsByClassName("sumit" + String(i))[0]) {
                     // on mouseout, clear all summary boxes
                     clearDivs();
@@ -103,27 +103,23 @@ function addFunction() {
 }
 
 function initialize() {
-    chrome.storage.sync.get(["id"], function(result) {
+    chrome.storage.sync.get(["id"], function (result) {
         apiID = result.id;
     });
-    chrome.storage.sync.get(["key"], function(result) {
+    chrome.storage.sync.get(["key"], function (result) {
         apiKey = result.key;
     });
-    chrome.storage.sync.get(["cl"], function(result) {
+    chrome.storage.sync.get(["cl"], function (result) {
         cl = result.cl;
     });
-    chrome.storage.sync.get(["whitelist"], function(result) {
+    chrome.storage.sync.get(["whitelist"], function (result) {
         urls = result.whitelist;
     });
     // timeout to allow time for chrome to sync url values
     let documentURL = new URL(document.URL);
-    setTimeout(function() {
-        if (!urls) {
-            urls = '';
-        }
-        if (urls.includes(documentURL.hostname) === false && documentURL.hostname !== '') {
-            addFunction();
-        }
+    setTimeout(function () {
+        if (!urls) { urls = '' };
+        if (urls && urls.includes(documentURL.hostname) || urls === '*') { addFunction() };
     }, 10);
 }
 
--- popup.js
+++ popup.js
@@ -1,35 +1,35 @@
-document.getElementById("id").onchange = function() {
-    chrome.storage.sync.set({"id": this.value});
+document.getElementById("id").onchange = function () {
+    chrome.storage.sync.set({ "id": this.value });
 };
 
-document.getElementById("key").onchange = function() {
-    chrome.storage.sync.set({"key": this.value});
+document.getElementById("key").onchange = function () {
+    chrome.storage.sync.set({ "key": this.value });
 };
 
-document.getElementById("whitelist").onchange = function() {
-    chrome.storage.sync.set({"whitelist": this.value});
+document.getElementById("whitelist").onchange = function () {
+    chrome.storage.sync.set({ "whitelist": this.value });
 };
 
-chrome.storage.sync.get(["id"], function(result) {
+chrome.storage.sync.get(["id"], function (result) {
     if (result.id) {
         document.getElementById("id").value = result.id;
     }
 });
 
-chrome.storage.sync.get(["key"], function(result) {
+chrome.storage.sync.get(["key"], function (result) {
     if (result.key) {
         document.getElementById("key").value = result.key;
     }
 });
 
-chrome.storage.sync.get(["cl"], function(result) {
-    if (result.cl) {
-        document.getElementById("sl").innerHTML = "Searches left: " + result.cl;
+chrome.storage.sync.get(["whitelist"], function (result) {
+    if (result.whitelist) {
+        document.getElementById("whitelist").value = result.whitelist;
     }
 });
 
-chrome.storage.sync.get(["whitelist"], function(result) {
-    if (result.whitelist) {
-        document.getElementById("whitelist").value = result.whitelist;
+chrome.storage.sync.get(["cl"], function (result) {
+    if (result.cl) {
+        document.getElementById("sl").innerHTML = "Searches left: " + result.cl;
     }
 });
